a95c5687176f2f3b0d2f5ad7287e648e472db56f
Brandon <143046330+BCrenshaw@users.noreply.github.com>
2023-09-07 00:41:49 +0000

    Upload Assignment: Homework 1, Due Date 09/06/23

--- tests/homework/b_in_proc_out/tests_in_proc_out.py
+++ tests/homework/b_in_proc_out/tests_in_proc_out.py
@@ -5,6 +5,7 @@ The file at /src/homework/b_in_proc_out/output has
 the function get_number.
 '''
 from src.homework.b_in_proc_out.output import get_number
+from src.homework.b_in_proc_out.output import multiply_numbers
 
 class Test_Config(unittest.TestCase):
 
@@ -16,3 +17,13 @@ class Test_Config(unittest.TestCase):
         #test that the function get_number returns 2
         self.assertEqual(2, get_number(2))
 
+    def test_multiply_numbers_1(self):
+        self.assertEqual(25, multiply_numbers(5, 5))
+
+    def test_multiply_numbers_2(self): 
+        self.assertEqual(100, multiply_numbers(10, 10))
+'''
+The "Write Unit Test" section specified to name the 5 * 5 = 25 test case as 'test_multiply_numbers_1' in section a.
+Then section b seems to state creating a new function as 'test_multiply_numbers_1' again for the 10 * 10 = 100 test.
+So that I could see the result when the test is ran I named the second test function 'test_multiply_numbers_2'. I hope this doesn't make me down.
+'''
