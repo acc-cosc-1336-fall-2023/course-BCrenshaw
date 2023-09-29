21183579d92fdc218c7a51ca54aeef4acd6d997f
Brandon <143046330+BCrenshaw@users.noreply.github.com>
2023-09-29 02:30:01 +0000

    Homework 4 Specific Seconds Time Conv. 20230928

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,7 @@ YOU HAVE TO TEST FROM HERE BRANDON!
 #from tests.examples.a_example import tests_input_process_output
 #from tests.examples.c_decisions import tests_decisions
 #from tests.examples.d_repetition import tests_repetition
-from tests.examples.e_functions import tests_functions
+#from tests.examples.e_functions import tests_functions
 #from tests.examples.f_files_exception import tests_files_exception
 #from tests.examples.g_lists_and_tuples import tests_lists_and_tuples
 #from tests.examples.h_strings import tests_strings
@@ -21,7 +21,7 @@ from tests.examples.e_functions import tests_functions
 #from tests.homework.b_in_proc_out import tests_in_proc_out
 #from tests.homework.c_decisions import tests_decisions
 #from tests.homework.d_repetition import tests_repetition
-#from tests.homework.e_functions import tests_functions
+from tests.homework.e_functions import tests_functions
 #from tests.homework.f_files_exception import tests_files_exception
 #from tests.homework.g_lists_and_tuples import tests_lists_and_tuples
 #from tests.homework.h_strings import tests_strings
