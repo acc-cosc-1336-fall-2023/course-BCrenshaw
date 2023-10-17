d6bc262784699a29790cda555c8b5556ed54aff6
Brandon <143046330+BCrenshaw@users.noreply.github.com>
2023-10-17 00:18:11 +0000

    Getting lost with lists v20231016

--- src/examples/g_lists_and_tuples/lists.py
+++ src/examples/g_lists_and_tuples/lists.py
@@ -1,2 +1,27 @@
 def test_config():
     return True
+
+def get_list_total_while(list1):
+    index = 0
+    sum = 0
+    
+    while(index < len(list1)):
+        sum += list1[index]
+        index += 1
+
+    return sum
+        
+
+def get_list_total_for(list1):
+    sum = 0
+
+    for i in range(0, len(list1)):
+        sum += list1[i]
+
+    return sum
+
+def get_list_return_value(list1):
+    print(list1)
+    list[0] = 0
+
+    return list1
--- tests/examples/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/examples/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,9 +1,19 @@
 import unittest
 
-from src.examples.g_lists_and_tuples.lists import test_config
+from src.examples.g_lists_and_tuples.lists import get_list_total_for, get_list_total_while, test_config
 
 class Test_Config(unittest.TestCase):
 
     def test_configuration(self):
         self.assertEqual(True, test_config())
+
+    def test_get_list_total_while(self):
+        list1 = [5, 10, 20]
+        self.assertEqual(get_list_total_while(list1), 35)
+
+    def test_get_list_total_for(self):
+        list1 = [5, 10, 20]
+        self.assertEqual(get_list_total_for(list1), 35)
+
+
 
